44b31207f668ab21c8ee1f754bd3c3d02e1ef645
Sahil Naikdhure <sanaikdhure@gmail.com>
2023-12-21 12:45:30 +0000

    final touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,16 +26,169 @@ export class AppComponent implements OnInit {
   title = 'health-viz';
   @ViewChild("chart") chart: any;
   public chartOptions: any;
+  public barChartOptions: any;
   public dayCollection: any = {}
+  public monthCollection: any = {}
+  public monthAgg: any = {}
   public months: string[] = [
     "Jan", 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
   ]
+  public stepArr : number[] = []
+  public distanceArr: number[] = []
+  public calorieArr: number[] = []
+  public monthAggArr: string[] = []
   public chartLoaded: boolean = false;
 
   constructor(private http: HttpClient) {
     
   }
+
+  updateDisplay() {
+    this.chart.updateOptions({
+      series: [
+        {
+          name: "Sun",
+          data: this.dayCollection[0].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        },
+        {
+          name: "Mon",
+          data: this.dayCollection[1].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        },
+        {
+          name: "Tue",
+          data: this.dayCollection[2].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        },
+        {
+          name: "Wed",
+          data: this.dayCollection[3].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        },
+        {
+          name: "Thu",
+          data: this.dayCollection[4].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        },
+        {
+          name: "Fri",
+          data: this.dayCollection[5].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        },
+        {
+          name: "Sat",
+          data: this.dayCollection[6].map((obj: any) => {
+            return {
+              x: obj.month,
+              y: obj[this.displayType]
+            }
+          })
+        }
+      ]
+    }, true)
+
+    if(this.displayType === 'calorie') {
+      this.chart.updateOptions({
+        plotOptions: {
+          heatmap: {
+            shadeIntensity: 0.5,
+            colorScale: {
+              ranges: [
+                {
+                  from: 0,
+                  to: 50,
+                  name: "low",
+                  color: "#00A100"
+                },
+                {
+                  from: 51,
+                  to: 100,
+                  name: "medium",
+                  color: "#128FD9"
+                },
+                {
+                  from:101,
+                  to: 250,
+                  name: "high",
+                  color: "#FFB200"
+                },
+                {
+                  from: 251,
+                  to: 500,
+                  name: "extreme",
+                  color: "#FF0000"
+                }
+              ]
+            }
+          }
+        }
+      },true)
+    } else {
+      this.chart.updateOptions({
+        plotOptions: {
+          heatmap: {
+            shadeIntensity: 0.5,
+            colorScale: {
+              ranges: [
+                {
+                  from: 0,
+                  to: 1500,
+                  name: "low",
+                  color: "#00A100"
+                },
+                {
+                  from: 1501,
+                  to: 3000,
+                  name: "medium",
+                  color: "#128FD9"
+                },
+                {
+                  from:3001,
+                  to: 8000,
+                  name: "high",
+                  color: "#FFB200"
+                },
+                {
+                  from: 8001,
+                  to: 20000,
+                  name: "extreme",
+                  color: "#FF0000"
+                }
+              ]
+            }
+          }
+        }
+      }, true)
+    }
+  }
+
+  public displayType = 'step'
 
   public dataObj: any = {}
   ngOnInit() {
@@ -45,35 +198,58 @@ export class AppComponent implements OnInit {
         for (let index = 1; index < csvToRowArray.length - 1; index++) {
           let row = csvToRowArray[index].split(",");
           this.dataObj[row[4]] = {
-            // god i'm so good at coding
             step: row[0],
             speed: row[1],
             distance: row[2],
             calorie: row[3]
           }
-          let day = new Date(+row[4]).getDay()
-
-          if(this.dayCollection[day]) {
-            this.dayCollection[day].push({steps: row[0], month: this.months[new Date(+row[4]).getMonth()]})
-          } else {
-            this.dayCollection[day] = [];
-            this.dayCollection[day].push({steps: row[0], month: this.months[new Date(+row[4]).getMonth()]})
-          }
         }
 
-        Object.keys(this.dayCollection).forEach(k => {
-          if(this.dayCollection[k].length < 78) {
-            let diff = 78 - this.dayCollection[k].length
-            console.log(diff)
-
-            for(let i = 0; i < diff; i++) {
-              this.dayCollection[k].push({
-                steps: 0,
-                month: this.months[11]
-              })
-            }
+        Object.keys(this.dataObj).forEach(k => {
+          let day = new Date(+k).getDay()
+          if(this.dayCollection[day]) {
+            this.dayCollection[day].push({steps: this.dataObj[k].step, month: this.months[new Date(+k).getMonth()], distance: this.dataObj[k].distance, calorie: this.dataObj[k].calorie})
+          } else {
+            this.dayCollection[day] = [];
+            this.dayCollection[day].push({steps: this.dataObj[k].step, month: this.months[new Date(+k).getMonth()], distance: this.dataObj[k].distance, calorie: this.dataObj[k].calorie})
           }
-          
+
+          let month = new Date(+k).getMonth()
+          if(this.monthCollection[month]) {
+            this.monthCollection[month].push({
+              step: this.dataObj[k].step,
+              speed: this.dataObj[k].speed,
+              distance: this.dataObj[k].distance,
+              calorie: this.dataObj[k].calorie
+            })
+          } else {
+            this.monthCollection[month] = []
+            this.monthCollection[month].push({
+              step: this.dataObj[k].step,
+              speed: this.dataObj[k].speed,
+              distance: this.dataObj[k].distance,
+              calorie: this.dataObj[k].calorie
+            })
+          }
+        })
+
+        Object.keys(this.monthCollection).forEach((m: any) => {
+          let aggObj = {
+            step: 0,
+            distance: 0,
+            calorie: 0
+          }
+          this.monthCollection[m].forEach((mObj: any) => {
+            aggObj.step += +mObj.step
+            aggObj.distance += +mObj.distance
+            aggObj.calorie += +mObj.calorie
+          })
+
+          this.stepArr.push(aggObj.step)
+          this.distanceArr.push(aggObj.distance)
+          this.calorieArr.push(aggObj.calorie)
+          this.monthAggArr.push(this.months[m])
+          this.monthAgg[this.months[m]] = aggObj
         })
 
         this.chartOptions = {
@@ -182,9 +358,52 @@ export class AppComponent implements OnInit {
           },
           dataLabels: {
             enabled: false
+          }
+        };
+
+        this.barChartOptions = {
+          series: [
+            {
+              name: "Steps",
+              data: this.stepArr
+            },
+            {
+              name: "Distance",
+              data: this.distanceArr
+            },
+            // {
+            //   name: "Calories",
+            //   data: this.calorieArr
+            // }
+          ],
+          chart: {
+            type: "bar",
+            height: 430,
+            width: 1050
           },
-          title: {
-            text: "Step Count"
+          plotOptions: {
+            bar: {
+              horizontal: true,
+              dataLabels: {
+                position: "top"
+              }
+            }
+          },
+          dataLabels: {
+            enabled: true,
+            offsetX: -6,
+            style: {
+              fontSize: "12px",
+              colors: ["#fff"]
+            }
+          },
+          stroke: {
+            show: true,
+            width: 1,
+            colors: ["#fff"]
+          },
+          xaxis: {
+            categories: this.monthAggArr
           }
         };
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgApexchartsModule } from 'ng-apexcharts';
+import {ButtonsModule} from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NgApexchartsModule } from 'ng-apexcharts';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgApexchartsModule
+    NgApexchartsModule,
+    FormsModule,
+    ButtonsModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
